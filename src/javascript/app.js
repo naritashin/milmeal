2e2050f138e8afca5f3fad9d517390d5f73bc8c8
SHIN_NARITA <triplfjump1417@gmail.com>
2017-10-30 19:59:34 +0900

    2017-10-30 jQueryを修正

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,24 +1,12 @@
 $(function() {
-  var $checkbox = $('.js-category');
-  var $products = $('.js-products');
+  var $productsList = $('.js-product_list');
 
   $.ajax({
     url: '../../api/milmeal.json',
-    dataType: 'json',
+    dataType: 'json'
   })
   .done(function(data) {
-    var $productList = $('.js-products');
-    var productArr = data.products;
-    var len = productArr.length;
-
-    for (var i = 0; i < len; i ++) {
-      var name = productArr[i].name;
-      var path = productArr[i].image;
-      var price = productArr[i].price;
-      var category = productArr[i].category;
-
-      $productList.append('<li class="js-'+ category +'"><a href="" class="p-img p-img-shadow u-inlineblock"><div class="p-img-product" style="background-image: url('+ path +');"></div><div class="c-text-basic c-text-bold p-img-cap u-block u-align-left"><p class="p-img-cap-text">'+ name +'</p><p class="c-text-essential c-text-light u-block u-align-right u-fs-14">販売価格(税込)： '+ price +'円</p></div></a></li>');
-    }
+    showProducts(data);
   })
   .fail(function() {
     console.log("error");
@@ -27,23 +15,37 @@ $(function() {
     console.log("complete");
   });
 
-  $checkbox.on('change', function() {
-    var $checked = $('.js-category input:checked');
-    var productList = $products.find('li');
-    var value = $checked.map(function(){
-      return $(this).val();
+  function showProducts(data) {
+    data.products
+    .forEach(function(products) {
+      item =
+        '<li class="js-'+ products.category +'">'+
+          '<a href="" class="p-img p-img-shadow u-inlineblock">'+
+            '<div class="p-img-product" style="background-image: url('+ products.img +');"></div>'+
+            '<div class="c-text-basic c-text-bold p-img-cap u-block u-align-left">'+
+              '<p class="p-img-cap-text">'+ products.name +'</p>'+
+              '<p class="c-text-essential c-text-light u-block u-align-right u-fs-14">販売価格(税込)： '+ products.price +'円</p>'+
+            '</div>'+
+          '</a>'+
+        '</li>';
+
+        $productsList.append(item);
     });
-    var len = value.length;
+  }
 
-    if(len) {
-      productList.hide();
-    } else {
-      productList.show();
-    };
+  $('.js-category').on('change', function() {
+    var value = Array.from(
+          $('.js-category input:checked')
+          .map(function(){
+            return $(this).val();
+          })
+        );
 
-    for (var i = 0; i < len; i++) {
-        $('.js-'+ value[i]).show();
-    };
+    $productsList.children().hide();
+
+    value.forEach(function(value) {
+      $('.js-' + value).show();
+    });
   });
 });
 
